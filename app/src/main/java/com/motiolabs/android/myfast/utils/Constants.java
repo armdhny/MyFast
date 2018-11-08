2b167caff23172e1a9ff8ad69f0dd34ab0c084ce
Bayu Ariantika Irsan <bayuariantika@gmail.com>
2018-11-08 23:45:22 +0700

    fix drawer & add splash

--- app/src/main/java/com/motiolabs/android/myfast/utils/Constants.java
+++ app/src/main/java/com/motiolabs/android/myfast/utils/Constants.java
@@ -12,69 +12,18 @@ public class Constants {
     private Constants() {
     }
 
-    /**  Extract the key associated with the JSONObject */
-    static final String JSON_KEY_RESPONSE = "response";
-    static final String JSON_KEY_RESULTS = "results";
-    static final String JSON_KEY_WEB_TITLE = "webTitle";
-    static final String JSON_KEY_SECTION_NAME = "sectionName";
-    static final String JSON_KEY_WEB_PUBLICATION_DATE = "webPublicationDate";
-    static final String JSON_KEY_WEB_URL = "webUrl";
-    static final String JSON_KEY_TAGS = "tags";
-    static final String JSON_KEY_FIELDS = "fields";
-    static final String JSON_KEY_THUMBNAIL = "thumbnail";
-    static final String JSON_KEY_TRAIL_TEXT = "trailText";
+    public final static String fast_server = "http://api.fast.or.id/";
+    public final static String server_addr = fast_server + "index.php/api/";
+    public final static String url_login = server_addr + "login";
 
-    /** Read timeout for setting up the HTTP request */
-    static final int READ_TIMEOUT = 10000; /* milliseconds */
+    public final static String log_app = "fast";
 
-    /** Connect timeout for setting up the HTTP request */
-    static final int CONNECT_TIMEOUT = 15000; /* milliseconds */
-
-    /** HTTP response code when the request is successful */
-    static final int SUCCESS_RESPONSE_CODE = 200;
-
-    /** Request method type "GET" for reading information from the server */
-    static final String REQUEST_METHOD_GET = "GET";
-
-    /** URL for news data from the guardian data set */
-    public static final String NEWS_REQUEST_URL = "https://content.guardianapis.com/search";
-
-    /** Parameters */
-    public static final String QUERY_PARAM = "q";
-    public static final String ORDER_BY_PARAM = "order-by";
-    public static final String PAGE_SIZE_PARAM = "page-size";
-    public static final String ORDER_DATE_PARAM = "order-date";
-    public static final String FROM_DATE_PARAM = "from-date";
-    public static final String SHOW_FIELDS_PARAM = "show-fields";
-    public static final String FORMAT_PARAM = "format";
-    public static final String SHOW_TAGS_PARAM = "show-tags";
-    public static final String API_KEY_PARAM = "api-key";
-    public static final String SECTION_PARAM = "section";
-
-    /** The show fields we want our API to return */
-    public static final String SHOW_FIELDS = "thumbnail,trailText";
-
-    /** The format we want our API to return */
-    public static final String FORMAT = "json";
-
-    /** The show tags we want our API to return */
-    public static final String SHOW_TAGS = "contributor";
-
-    /** API Key */
-    public static final String API_KEY = "test"; // Use your API Key when API rate limit exceeded
-
-    /** Default number to set the image on the top of the textView */
-    public static final int DEFAULT_NUMBER = 0;
-
-    /** Constants value for each fragment */
-    public static final int HOME = 0;
-    public static final int WORLD = 1;
-    public static final int SCIENCE = 2;
-    public static final int SPORT = 3;
-    public static final int ENVIRONMENT = 4;
-    public static final int SOCIETY = 5;
-    public static final int FASHION = 6;
-    public static final int BUSINESS = 7;
-    public static final int CULTURE = 8;
+    public final static String fast_shared = "fastsharedpref";
+    public final static String spref_pversi = "param_versi";
+    public final static String spref_ouid = "fast_uid";
+    public final static String spref_osession = "fast_session";
+    public final static String spref_name = "fast_name";
+    public final static String spref_email = "fast_email";
+    public final static String spref_avatar = "fast_avatar";
 
 }
